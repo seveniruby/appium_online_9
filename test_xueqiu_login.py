5a0e7fa80eb4abc9f7e0a373b00edf203fcaa85e
seveniruby <seveniruby@gmail.com>
2019-05-11 17:21:50 +0800

    add setup and teardwon demo

--- test_xueqiu_login.py
+++ test_xueqiu_login.py
@@ -9,59 +9,42 @@ from appium.webdriver.common.touch_action import TouchAction
 from appium.webdriver.webdriver import WebDriver
 
 
-class TestXueqiuAndroid(object):
+class TestXueqiuAndroidLogin(object):
+    driver = WebDriver
     @classmethod
     def setup_class(cls):
         print("setup class 在当前类下的所有用例执行之前只执行一次")
-        cls.install_app()
+        cls.driver=cls.install_app()
+
+        #进入我的页面
+        el1 = cls.driver.find_element_by_id("user_profile_icon")
+        el1.click()
+
 
     def setup_method(self):
         print("setup method 在每个测试用例执行之前执行一次")
         #获取启动的appium的driver实例，用于后续每个case的driver
-        self.driver=self.restart_app()
+        self.driver=TestXueqiuAndroidLogin.driver
 
-    def test_login(self):
-        el1 = self.driver.find_element_by_id("user_profile_icon")
-        el1.click()
+        #每次都会执行一次进入登录页的点击
         el2 = self.driver.find_element_by_id("tv_login")
         el2.click()
+
+
+
+    def test_login_phone(self):
         el3 = self.driver.find_element_by_id("tv_login_by_phone_or_others")
         el3.click()
 
+    def test_login_password(self):
+        el3 = self.driver.find_element_by_id("tv_login_by_phone_or_others")
+        el3.click()
+        self.driver.find_element_by_xpath("//*[@text='邮箱手机号密码登录']").click()
 
-    def test_found(self):
-        self.driver.find_element_by_xpath("//*[contains(@resource-id,'indicator')]//*[@text='基金']").click()
-
-    def test_swipe(self):
-        self.driver.find_element_by_xpath("//*[contains(@resource-id,'indicator')]//*[@text='基金']")
-        for i in range(5):
-            self.driver.swipe(1000, 1000, 200, 200)
-            time.sleep(2)
-
-    def test_action(self):
-        self.driver.find_element_by_xpath("//*[contains(@resource-id,'indicator')]//*[@text='基金']")
-        action=TouchAction(self.driver)
-        for i in range(5):
-            action.press(x=1000, y=1000).move_to(x=200, y=200).release().perform()
-            time.sleep(2)
-
-    def test_action_p(self):
-        rect=self.driver.get_window_rect()
-        self.driver.find_element_by_xpath("//*[contains(@resource-id,'indicator')]//*[@text='基金']")
-        action = TouchAction(self.driver)
-        for i in range(5):
-            action\
-                .press(x=rect['width']*0.8, y=rect['height']*0.8).move_to(x=rect['width']*0.2, y=rect['height']*0.2)\
-                .release()\
-                .perform()
-            time.sleep(2)
-
-    def test_window_size(self):
-        print(self.driver.get_window_rect())
 
     def teardown_method(self):
         #不加也没关系，如果不quit，启动appium会自动quit之前的session
-        self.driver.quit()
+        self.driver.back()
 
     @classmethod
     def install_app(cls) -> WebDriver:
