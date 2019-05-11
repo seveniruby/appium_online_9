3319a1ae5cb28b62f8cf7e986710864d58ca3214
seveniruby <seveniruby@gmail.com>
2019-05-11 17:03:18 +0800

    clean and comment

--- test_xueqiu.py
+++ test_xueqiu.py
@@ -10,32 +10,27 @@ from appium.webdriver.webdriver import WebDriver
 
 
 class TestXueqiuAndroid(object):
-    driver=WebDriver
     @classmethod
     def setup_class(cls):
-        print("setup class")
-
-        #cls.driver =cls.init_appium()
+        print("setup class 在当前类下的所有用例执行之前只执行一次")
+        cls.install_app()
 
     def setup_method(self):
-        print("setup method")
-
-        TestXueqiuAndroid.driver=self.restart_appium()
-        self.driver = TestXueqiuAndroid.driver
+        print("setup method 在每个测试用例执行之前执行一次")
+        #获取启动的appium的driver实例，用于后续每个case的driver
+        self.driver=self.restart_app()
 
     def test_login(self):
-        el1 = TestXueqiuAndroid.driver.find_element_by_id("user_profile_icon")
+        el1 = self.driver.find_element_by_id("user_profile_icon")
         el1.click()
-        el2 = TestXueqiuAndroid.driver.find_element_by_id("tv_login")
+        el2 = self.driver.find_element_by_id("tv_login")
         el2.click()
-        el3 = TestXueqiuAndroid.driver.find_element_by_id("tv_login_by_phone_or_others")
+        el3 = self.driver.find_element_by_id("tv_login_by_phone_or_others")
         el3.click()
 
 
-    def test_基金(self):
-
-        TestXueqiuAndroid.driver.find_element_by_xpath("//*[contains(@resource-id,'indicator')]//*[@text='基金']").click()
-
+    def test_found(self):
+        self.driver.find_element_by_xpath("//*[contains(@resource-id,'indicator')]//*[@text='基金']").click()
 
     def test_swipe(self):
         self.driver.find_element_by_xpath("//*[contains(@resource-id,'indicator')]//*[@text='基金']")
@@ -65,15 +60,19 @@ class TestXueqiuAndroid(object):
         print(self.driver.get_window_rect())
 
     def teardown_method(self):
-        TestXueqiuAndroid.driver.quit()
+        #不加也没关系，如果不quit，启动appium会自动quit之前的session
+        self.driver.quit()
 
     @classmethod
-    def init_appium(cls) -> WebDriver:
+    def install_app(cls) -> WebDriver:
         caps = {}
+        #如果有必要，进行第一次安装
+        # caps["app"]=''
         caps["platformName"] = "android"
         caps["deviceName"] = "hogwarts"
         caps["appPackage"] = "com.xueqiu.android"
         caps["appActivity"] = ".view.WelcomeActivityAlias"
+        #解决第一次启动的问题
         caps["autoGrantPermissions"] = "true"
         # caps['noReset']=True
 
@@ -82,14 +81,14 @@ class TestXueqiuAndroid(object):
         return driver
 
     @classmethod
-    def restart_appium(cls) -> WebDriver:
+    def restart_app(cls) -> WebDriver:
         caps = {}
-        #caps["app"]=''
+
         caps["platformName"] = "android"
         caps["deviceName"] = "hogwarts"
         caps["appPackage"] = "com.xueqiu.android"
         caps["appActivity"] = ".view.WelcomeActivityAlias"
-        #caps["autoGrantPermissions"] = "true"
+        #为了更快的启动，并保留之前的数据，从而可以保存上一个case执行后的状态
         caps['noReset']=True
 
         driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
